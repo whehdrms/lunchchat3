fcda3efb05d4b1b2ee540151f5b800c9dbd1feb9
Ubuntu <ubuntu@ip-172-31-8-11.ap-northeast-2.compute.internal>
2017-11-07 10:08:27 +0000

    first

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongodb://~~~~~/db명 으로 접속
-mongoose.connect('mongodb://localhost:27777/lunchting', {
+mongoose.connect('mongodb://localhost:27017/lunchting', {
   useMongoClient: true
 });
 
